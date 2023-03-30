021947f290776bb29e8fe626d4a3d7ac6c7e36c6
d1xogus <dlxo1012@naver.com>
2023-03-30 12:39:09 +0900

    api

--- demo/src/main/java/com/example/demo/api/articleapicontroller.java
+++ demo/src/main/java/com/example/demo/api/articleapicontroller.java
@@ -36,22 +36,19 @@ public class articleapicontroller {
                 ResponseEntity.status(HttpStatus.OK).body(created) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
+    //patch
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody articleform dto) {
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
 
 
-//    //patch
-//
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody articleform dto){
-//        Article article = dto.toEntity();
-//        Article target = articleRepository.findById(id).orElse(null);
-//        if(target == null && id != article.getId()){
-//            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//        Article updated= articleRepository.save(article);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
+
 //    //delect
 //    @DeleteMapping("/api/articles/{id}")
 //    public ResponseEntity<Article> delete(@PathVariable long id) {
--- demo/src/main/java/com/example/demo/service/ArticleService.java
+++ demo/src/main/java/com/example/demo/service/ArticleService.java
@@ -3,11 +3,13 @@ package com.example.demo.service;
 import com.example.demo.dto.articleform;
 import com.example.demo.entity.Article;
 import com.example.demo.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service //서비스 선언(서비스 객체를 스프링 부트에 생성)
 public class ArticleService {
     @Autowired
@@ -27,5 +29,23 @@ public class ArticleService {
             return null;
         }
         return articleRepository.save(article);
+    }
+
+    public Article update(Long id, articleform dto) {
+        // 1: DTO -> 엔티티
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2: 타겟 조회
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3: 잘못된 요청 처리
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+        // 4: 업데이트
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
     }
 }
